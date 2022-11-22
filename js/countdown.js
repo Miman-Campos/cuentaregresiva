27c8a7d12c0f79d3554943ae43ed960c00303abe
sergiomata <32809578+sergiomata@users.noreply.github.com>
2022-11-22 00:06:49 -0600

    feat: change words

--- js/countdown.js
+++ js/countdown.js
@@ -6,11 +6,10 @@ simplyCountdown('#cuenta', {
 	minutes: 31, // Default is 0 [0-59] integer
 	seconds: 0, // Default is 0 [0-59] integer
 	words: { //words displayed into the countdown
-		days: 'Día',
-		hours: 'Hora',
-		minutes: 'Minuto',
-		seconds: 'Segundo',
-		pluralLetter: 's'
+		days: { singular: 'Día', plural: 'Dias' },
+		hours: { singular: 'Hora', plural: 'Horas' },
+		minutes: { singular: 'Minuto', plural: 'Minutos' },
+		seconds: { singular: 'Segundo', plural: 'Segundos' }
 	},
 	plural: true, //use plurals
 	inline: false, //set to true to get an inline basic countdown like : 24 days, 4 hours, 2 minutes, 5 seconds
